f817b0edde2e24ff48835b17807c71470a7a3fa5
takeshi furusawa <djpdjp0517@gmail.com>
2020-07-30 07:39:43 +0000

    投稿の際、文章か画像のどちらかが存在しなければいけないというバリデーションを追加

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,5 @@
 class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
+  validates :image, presence: true, if: -> { content.blank? }
+  validates :content, presence: true, if: -> { image.blank? }
 end
