e4b927231791bd2b46eee64a0fa31112258720b1
takeshi furusawa <djpdjp0517@gmail.com>
2020-07-30 09:05:50 +0000

    アソシエーションの定義

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,4 +2,5 @@ class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :image, presence: true, if: -> { content.blank? }
   validates :content, presence: true, if: -> { image.blank? }
+  belongs_to :user
 end
